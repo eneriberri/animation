567e840835ebf5f9ae2b687bb5322bc50da78ca9
Irene Nguyen <irene.m.nguyen@gmail.com>
2014-04-13 12:56:57 -0700

    migrating to rails

---
+++ portfolio/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = 'ccec689c9246fb98a011dfceb26e10225e64ea8b88159003c2e34100cf5f4d40a2c29cef24f33252acce1d27dd5b87f47cb85885d0f0aceb25b9262801df8e9a'
